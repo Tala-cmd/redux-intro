b3511fdf67856788da6732488cfc45544cac7587
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-01-23 23:43:27 +0300

    Creating a redux store

--- src/store.jsx
+++ src/store.jsx
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance:0,
   loan:0,
@@ -14,8 +16,12 @@ function reducer(state= initialState, action){
 
     case 'account/requestLoan':
       if(state.loan > 0) return state;
-      //LATER
-      return {...state, loan: action.payload}
+      return {
+        ...state, 
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount
+      }
     
     case 'account/payLoan':
       return {
@@ -25,7 +31,20 @@ function reducer(state= initialState, action){
         balance: state.balance - state.loan
       }
     
-    case default:
+    default:
       return state;
   }
 }
+
+const store= createStore(reducer)
+
+store.dispatch({type: 'account/deposit', payload: 500})
+store.dispatch({type: 'account/withdraw', payload: 200})
+console.log(store.getState())
+
+store.dispatch({type: 'account/requestLoan', 
+payload:{amount: 1000, purpose: 'Buy a car'} })
+console.log(store.getState())
+
+store.dispatch({type: 'account/payLoan'})
+console.log(store.getState())
