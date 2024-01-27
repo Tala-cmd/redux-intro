63b5e86a892813a399607d9c56239f0331cad90a
Tala <70854429+Tala-cmd@users.noreply.github.com>
2024-01-27 23:38:21 +0300

    Connecting Redux App with React

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
 import App from './App.jsx'
 import './index.css'
 
 import store from './store.jsx'
 
-store.dispatch({type: 'account/deposit', payload: 500})
-console.log(store.getState())
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
